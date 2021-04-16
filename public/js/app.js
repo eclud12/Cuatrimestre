ee1c6a5975af80b3a9361a543b14cc79cb36d819
Saira Martinez <al221910498@gmail.com>
2021-04-16 12:38:40 -0500

    Crear un archivo vue exclusivo para adminlte

--- public/js/app.js
+++ public/js/app.js
@@ -49878,12 +49878,47 @@ var apicategory = new Vue({
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+
+/*require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+/*if (document.getElementById('app')) {
+   const app = new Vue({
+       el: '#app',
+   });
+
+}
+
+if (document.getElementById('apicategory')) {
+   require('./apicategory');
+}
+
+if (document.getElementById('confirmareliminar')) {
+   require('./confirmareliminar');
+}*/
+
 __webpack_require__(/*! ./tienda/bootstrap */ "./resources/js/tienda/bootstrap.js");
 
-__webpack_require__(/*! ./comun */ "./resources/js/comun.js");
+__webpack_require__(/*! ./comun */ "./resources/js/comun.js"); //require('./apicategory');
 
 /***/ }),
-
 
 /***/ "./resources/js/components/ExampleComponent.vue":
 /*!******************************************************!*\
@@ -49961,22 +49996,22 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-  window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
-  Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
-  
-  if (document.getElementById('app')) {
-    var app = new Vue({
-      el: '#app'
-    });
-  }
-  
-  if (document.getElementById('apicategory')) {
-    __webpack_require__(/*! ./admin/apicategory */ "./resources/js/admin/apicategory.js");
-  }
-  
-  if (document.getElementById('confirmareliminar')) {
-    __webpack_require__(/*! ./confirmareliminar */ "./resources/js/confirmareliminar.js");
-  }
+window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
+Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
+
+if (document.getElementById('app')) {
+  var app = new Vue({
+    el: '#app'
+  });
+}
+
+if (document.getElementById('apicategory')) {
+  __webpack_require__(/*! ./admin/apicategory */ "./resources/js/admin/apicategory.js");
+}
+
+if (document.getElementById('confirmareliminar')) {
+  __webpack_require__(/*! ./confirmareliminar */ "./resources/js/confirmareliminar.js");
+}
 
 /***/ }),
 
@@ -50010,42 +50045,42 @@ var confirmareliminar = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-  window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
-  /**
-   * We'll load jQuery and the Bootstrap jQuery plugin which provides support
-   * for JavaScript based Bootstrap features such as modals and tabs. This
-   * code may be modified to fit the specific needs of your application.
-   */
-  
-  try {
-    window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js")["default"]; // window.$ = window.jQuery = require('jquery');
-  
-    __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
-  } catch (e) {}
-  /**
-   * We'll load the axios HTTP library which allows us to easily issue requests
-   * to our Laravel back-end. This library automatically handles sending the
-   * CSRF token as a header based on the value of the "XSRF" token cookie.
-   */
-  
-  
-  window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-  window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-  /**
-   * Echo exposes an expressive API for subscribing to channels and listening
-   * for events that are broadcast by Laravel. Echo and event broadcasting
-   * allows your team to easily build robust real-time web applications.
-   */
-  // import Echo from 'laravel-echo';
-  // window.Pusher = require('pusher-js');
-  // window.Echo = new Echo({
-  //     broadcaster: 'pusher',
-  //     key: process.env.MIX_PUSHER_APP_KEY,
-  //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-  //     encrypted: true
-  // });
-  
-  /***/ }),
+window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+  window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js")["default"]; //window.$ = window.jQuery = require('jquery');
+
+  __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
+} catch (e) {}
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+
+window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+// import Echo from 'laravel-echo';
+// window.Pusher = require('pusher-js');
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     encrypted: true
+// });
+
+/***/ }),
 
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
